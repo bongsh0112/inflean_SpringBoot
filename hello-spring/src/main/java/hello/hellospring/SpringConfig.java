81c1f26f0e3ee341eb4ba402c01632cda452c867
bongsh0112 <sehwan@bongsehwan-ui-MacBookAir.local>
2023-05-25 03:12:09 +0900

    :technologist: 스프링 입문 // section6 JPA 전까지

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,38 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 @Configuration
 public class SpringConfig {
 
+    private final DataSource dataSource; // db와 연결할수있는 정보가 있는 데이터소스를 만들어줌
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource; // 데이터소스를 DI해줌
+    }
+
     @Bean
     public MemberService memberService() {
+
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -1,49 +1,34 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@Transactional
 
-class MemberServiceTest {
+class MemberServiceIntegrationTest {
 
-    MemberService memberService;
-    //클리어해줘야하는데 memberservice밖에 없음
+    @Autowired MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository; // test할때는 필드방식으로 오토와이어드 해도됨
 
-//    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
-    //main에 있는 그거 맞음 -> 다른 레포에있기때문에 인스턴스라던지 그런게 달라질 수 있다
-    //memberservice.java에서 만든 레포와 완전 다른 레포임 ㅠ
-    //같은 레포로 테스트하는게 맞지만 다른 레포로 테스트중인거 그래서 어떻게 하냐
-    //memberservice에서 constructor만들어서 외부에 넣어주도록
-
-    MemoryMemberRepository memberRepository;
-
-    @BeforeEach
-    public void BeforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        //여기서 만든 메모리멤버레포를
-        memberService = new MemberService(memberRepository);
-        //여기다가 넣어주니까 같은 메모리멤버레포를 쓰는게 됨
-        //memberservice 차원에서 보면 외부에서 파라미터를 넣어준다. 이런게 뭐냐면 dependency ijection(DI)
-    }
-
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
-
+    // afterEach가 필요했던 이유는 메모리의 데이터들을 지우기 위함이었는데 이제 db가 있으니까 필요없음
 
     @Test
     void 회원가입() {
         //given : 뭔가가 주어짐
         Member member = new Member();
-        member.setName("hello");
+        member.setName("spring");
 
         //when : 실행시
         Long saveId = memberService.join(member);
@@ -64,28 +49,10 @@ class MemberServiceTest {
         Member member2 = new Member();
         member2.setName("spring");
 
-        //when
-//        memberService.join(member1);
-//        try {
-//            memberService.join(member2);
-//            fail();
-//        } catch (IllegalStateException e) {
-//            Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.123");
-//        }
-
         memberService.join(member1); // join시 계속 메모리 DB에 누적되기때문에 항상 클리어해줘야함.
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
         Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-
-        //then
     }
 
-    @Test
-    void findMembers() {
-    }
-
-    @Test
-    void findOne() {
-    }
 }
