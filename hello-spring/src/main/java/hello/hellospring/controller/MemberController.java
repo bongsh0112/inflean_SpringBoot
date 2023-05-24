21b3b3f6fc28ef5c294b2df76d6e99dcb7d622fb
bongsh0112 <sehwan@bongsehwan-ui-MacBookAir.local>
2023-05-25 00:46:22 +0900

    :technologist: 스프링 입문 // section5 WebMVC 개발 완

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,30 @@ public class MemberController {
     @Autowired //컨트롤러와 서비스를 연결
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+//        System.out.println("member = " + form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/"; //회원가입이 끝났으니 홈화면으로 돌아감
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members); //members전체를 model에다 담아서 화면으로 아예 넘김
+
+        return "members/memberList";
     }
 }
