b86ea0fd79c2426482908d31adc3b5c06a1a1075
bongsh0112 <sehwan@bongsehwan-ui-MacBookAir.local>
2023-05-26 01:57:31 +0900

    :technologist: 스프링 입문 // section6 강의 완

--- hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository { //interface가 interface받을땐 extends
 
 
+    //JPQL select m from Member m where m.name = ? findByName(String name)만 보고 규칙을 따라서 저렇게 JPQL문을 만들어줌...ㄷㄷ
     @Override
     Optional<Member> findByName(String name);
 }
